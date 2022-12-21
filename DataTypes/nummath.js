e542375d94e6e35ea615a40ad859260b64503248
Your Name <you@example.com>
2022-12-21 10:57:00 +0700

    update all

---
+++ DataTypes/nummath.js
@@ -0,0 +1,19 @@
+let num1 = 100;
+
+//Basic Math
+console.log("********Basic Math********");
+console.log(num1 + 25);
+console.log(num1 - 100);
+console.log(num1 * 100);
+console.log(num1 / 1500);
+
+console.log("*******Additional MAthematical Operations********");
+//Additional Mathematcal Operations
+console.log(num1 % 1500); //REmainder
+console.log(++num1); //Tncrement
+console.log(--num1); //Decrement
+
+console.log("**************Using the Math Object*********");
+//Using the Math Object
+console.log(Math.PI);//PI
+console.log(MAth.sqrt(num1)); //square root
