6aec8902fe16f85ebf93b396d780e52bede1a85a
Your Name <you@example.com>
2022-12-21 09:42:08 +0700

    update all

---
+++ DataTypes/datatypes.js
@@ -0,0 +1,17 @@
+const people = ["Aaron" , "Mel" , "John" ] ;
+const one = 1;
+const str = "Hello World";
+const b =  trun;
+const employee = {
+    firstName: "Anirach",
+    lastName: "Mingkhwan",
+};
+
+function sayHello(person){
+    console.log("Hello" + person.firsName);
+}
+
+console.log(typeof people);
+console.log(typeof sayHello)
+console.log(employee instanceof Array);
+sayHello(employee);
---
+++ DataTypes/demovarletconst.js
@@ -0,0 +1,15 @@
+var hello = "Hello"
+
+console.log(hell);
+hello = "Hello World";
+console.log(hello);
+    
+    if (trun) {
+        let World = "Hello World";
+        console.log(world);
+    }
+    // console.log(world);
+
+    const fixval =  "Fix Value";
+    console.log(fixval);
+    // fixvar = "tes assigne value to const"
