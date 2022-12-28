18d454d8e72aa53778c877cdd910a0295c3611dc
Your Name <you@example.com>
2022-12-28 11:54:06 +0700

    update all

---
+++ DataTypes/HW00/hw1.js
@@ -0,0 +1,6 @@
+const findPerimeter = (a,b)=> (a+b) *2;
+
+console.log(findPerimeter(6,7));
+console.log(findPerimeter(20,10));
+console.log(findPerimeter(2,9));
+
--- DataTypes/arrowf-sample.js
+++ DataTypes/arrowf-sample.js
@@ -1,20 +1,21 @@
 //Arrow function with two arguments
-const sum = (firaParam , secondParam) =>{
-    return firsParam + secondParam;
+const sum = (firstParam , secondParam) =>{
+    return firstParam + secondParam;
 };
 console.log(sum(2,5)); //Prints:7
+
 //Arrow function with on arguments
 const printHello = () => {
-    console.log("hello")
+    console.log("hello");
 };
 printHello(); // Prints: hello
 
 //Arrow function with a single argument
 const checkWeinght = (weight) => {
     console.log(`Baggage weight : ${weight} kilograms`);
-}
+};
     checkWeinght(25); //
 
     //Concise arrow functions
     const multiply = (a,b) => a * b;
-    console.log(musltiply(2 , 30)); // Prints: 60
+    console.log(multiply(2 , 30)); // Prints: 60
