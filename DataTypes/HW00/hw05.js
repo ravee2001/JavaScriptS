7f7514fd4e7a88bca4b0e6d9087068c97c31bc19
Your Name <you@example.com>
2023-01-25 10:02:31 +0700

    update all

---
+++ DataTypes/HW00/hw05.js
@@ -0,0 +1,6 @@
+//numberSplit(4) -> (2,2)
+//numberSplit(10) -> (2,2)
+//numberSplit(11) -> (2,2)
+//numberSplit(-9)-> (-5,-4)
+
+console.log(Math.floor(4));
---
+++ DataTypes/HW00/hw06.js
@@ -0,0 +1,4 @@
+//highestDigit(379) -> 9
+//highestDigit(2) -> 2
+//highestDigit(377401) -> 7
+
