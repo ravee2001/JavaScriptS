1316baffb9ab19ad43b0d83d9c987e1ec63feecf
Your Name <you@example.com>
2023-01-25 11:15:38 +0700

    update all

--- DataTypes/HW00/hw05.js
+++ DataTypes/HW00/hw05.js
@@ -1,6 +1,21 @@
 //numberSplit(4) -> (2,2)
-//numberSplit(10) -> (2,2)
-//numberSplit(11) -> (2,2)
+//numberSplit(10) -> (5,5)
+//numberSplit(11) -> (5,6)
 //numberSplit(-9)-> (-5,-4)
 
-let 
+const numberSplit = (num) => {
+    if (num === undefined) {
+      return "Input Parameter can't be empty"
+    } else {
+    let a = Math.floor(num / 2);
+    let b = num - a;
+    let c = [];
+    c.push(a, b);
+    return c;
+    }
+  };
+  console.log(numberSplit(4));
+  console.log(numberSplit(10));
+  console.log(numberSplit(11));
+  console.log(numberSplit(-9));
+
